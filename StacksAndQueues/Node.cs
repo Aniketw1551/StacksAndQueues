9d04aa90db18db2962a712bb64b9289de4002acc
Aniketw1551 <aniketwaghmare976@gmail.com>
2022-01-29 20:58:33 +0530

    [Aniket] Adding CreatingStack program

---
+++ StacksAndQueues/Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    public class Node
+    {
+        //variables
+        public int data;
+        public Node next;
+        /// <summary>
+        /// Using constructor to initialized the data
+        /// </summary>
+        /// <param name="data">The data.</param>
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+}
---
+++ StacksAndQueues/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+              Console.WriteLine("Welcome to Stack and Queue program using Linked List");
+              StackandQueue();
+              Console.ReadLine();
+            }
+        }
+        //Method to Call the stack queue 
+        public static void StackandQueue()
+        {
+            LinkedListStack linkedListStack = new LinkedListStack();
+            bool CONTINUE = true;
+            while (CONTINUE)
+            {
+                Console.WriteLine("\nPlease choose your option");
+                Console.WriteLine("1.Stack 0.Exit");
+                int choose = int.Parse(Console.ReadLine());
+                switch (choose)
+                {
+                    case 1:
+                        Console.WriteLine("Creating A Stack");
+                        linkedListStack.Push(70);
+                        linkedListStack.Push(30);
+                        linkedListStack.Push(56);
+                        linkedListStack.Display();
+                        break;
+                    case 0:
+                        CONTINUE = false;
+                        break;
+                    default:
+                        Console.WriteLine("Check your option");
+                        break;
+                }
+            }
+        }
+    }
+}
+            
