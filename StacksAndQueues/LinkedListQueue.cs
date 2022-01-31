89d17bdc86948742fd796323036263789c75e813
Aniketw1551 <aniketwaghmare976@gmail.com>
2022-01-31 19:16:24 +0530

    [Aniket] Adding Dequeue from Queue program

--- StacksAndQueues/LinkedListQueue.cs
+++ StacksAndQueues/LinkedListQueue.cs
@@ -44,5 +44,45 @@ namespace StacksAndQueues
                 }
             }
         }
+        //Dequeues this instance
+        public void Dequeue()
+        {
+            if (this.front == null)
+            {
+                Console.WriteLine("Queue is Empty");
+                return;
+            }
+            Node temp = this.front;
+            this.front = front.next;
+            temp.next = null;
+            Console.WriteLine("{0} Dequeued from Queue", temp.data);
+        }
+        //Method to Copy elements into stack
+        public void CopyElementIntoStack()
+        {
+            int[] array = new int[6];
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(20);
+            queue.Enqueue(36);
+            queue.Enqueue(50);
+            queue.Enqueue(66);
+            queue.CopyTo(array, 0);
+            foreach (int item in array)
+            {
+                Console.WriteLine("Element in Array:{0}", item);
+            }
+            Stack<int> stack = new Stack<int>();
+            Console.WriteLine("\nCopy array element into stack\n");
+            foreach (int element in array)
+            {
+                if (element != 0)
+                    stack.Push(element);
+            }
+            foreach (int element in stack)
+            {
+                if (element != 0)
+                    Console.WriteLine("Element in stack:{0}", element);
+            }
+        }
     }
 }
--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -24,13 +24,13 @@ namespace StacksAndQueues
             bool CONTINUE = true;
             while (CONTINUE)
             {
-                Console.WriteLine("\nPlease choose your option");
+                Console.WriteLine("Please choose your option\n");
                 Console.WriteLine("1.Stack 2.Queue 0.Exit");
                 int choose = int.Parse(Console.ReadLine());
                 switch (choose)
                 {
                     case 1:
-                        Console.WriteLine("Creating a Stack\n");
+                        Console.WriteLine("\nCreating a Stack\n");
                         linkedListStack.Push(70);
                         linkedListStack.Push(30);
                         linkedListStack.Push(56);
@@ -46,11 +46,16 @@ namespace StacksAndQueues
                         linkedListStack.Display();
                         break;
                     case 2:
-                        Console.WriteLine("Creating a Queue\n");
+                        Console.WriteLine("\nCreating a Queue\n");
                         linkedListQueue.Enqueue(56);
                         linkedListQueue.Enqueue(30);
                         linkedListQueue.Enqueue(70);
                         linkedListQueue.Display();
+                        Console.WriteLine("\nDequeue from Queue\n");
+                        linkedListQueue.Dequeue();
+                        linkedListQueue.Display();
+                        Console.WriteLine("\nCopy element from Queue into Stack\n");
+                        linkedListQueue.CopyElementIntoStack();
                         break;
                     case 0:
                         CONTINUE = false;
